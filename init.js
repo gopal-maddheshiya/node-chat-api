3f6a1bd402175c3060aa5707ae6da0dceab9af0d
Gopal96216 <gopalmaddheshiya138@gmail.com>
2025-11-10 15:45:38 +0530

    Initial commit - demo Node.js MongoDB project

---
+++ init.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main().then(() => {
+  console.log("connection succesful");
+})
+  .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let allChats = [
+  {
+    from: "Aadarsh",
+    to: "Gopal",
+    msg: "Ok bro I will share my examsheet to you",
+    create_at: new Date(),
+  },
+  {
+    from: "Sandesh",
+    to: "Aadarsh",
+    msg: "Bro let's go our home for Diwali",
+    create_at: new Date(),
+  },
+  {
+     from: "Gopal",
+    to: "Arun",
+    msg: "Bro I am on my way",
+    create_at: new Date(),
+  },
+  {
+     from: "Shrijaya",
+    to: "Gopal",
+    msg: "Where are you now?",
+    create_at: new Date(),
+  },
+  {
+     from: "Gopal",
+    to: "Shrijaya",
+    msg: "I am now in my hostel room",
+    create_at: new Date(),
+  }
+];
+
+Chat.insertMany(allChats);
